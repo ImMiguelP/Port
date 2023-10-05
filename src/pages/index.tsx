71dcf55eb3c888142119d8608046168b2e5aa49a
ImMiguelP <anarchxzx@gmail.com>
2023-10-04 22:06:56 -0700

    chore: filled in lorem ipson

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import {  Center, Heading,Stack } from '@chakra-ui/react'
+import {  Center, Heading, Stack, Link as ChakraLink   } from '@chakra-ui/react'
 import Technologies from '@/components/Technologies/Technologies'
 import Contact from '@/components/Contacts/Contact'
 import Projects from '@/components/ProjectInfo/Projects'
@@ -14,7 +14,9 @@ export default function Home() {
         Miguel
       </Heading>
       <Heading as={"h5"} size={"sm"}>
-        28 year old with 4+ years of front web development.
+        28 year old with 4+ years of front web development. Currently building <ChakraLink href="https://musy.one" textDecoration="underline" color="blue">
+    musy
+  </ChakraLink>
       </Heading>
       <Heading pt={"12px"}>
         Technologies
