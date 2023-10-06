582b4d6983d622dc7b6d09d47e2a43acab4af21b
ImMiguelP <anarchxzx@gmail.com>
2023-10-05 17:38:02 -0700

    feat: toggle bg color button

--- src/components/NavBar/Nav.tsx
+++ src/components/NavBar/Nav.tsx
@@ -1,8 +1,10 @@
-import { HStack, Text } from '@chakra-ui/react'
+import { Button, HStack, Text, useColorMode } from '@chakra-ui/react'
 import Link from 'next/link'
 import React from 'react'
+import { FaMoon, FaSun } from 'react-icons/fa6';
 
 const Nav = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
   return (
     <HStack mx={5}  justifyContent={"space-between"}>
     {/* THis is where a logo would go if i had one so for now it will just be my name */}
@@ -13,6 +15,9 @@ const Nav = () => {
         <HStack>
         <Text>test</Text>
         <Text>test</Text>
+        <Button variant="ghost" _hover={{ backgroundColor: "transparent" }} onClick={toggleColorMode}>
+        {colorMode === "light" ? <FaMoon /> : <FaSun />}
+    </Button>
         </HStack>
     </HStack>
   )
