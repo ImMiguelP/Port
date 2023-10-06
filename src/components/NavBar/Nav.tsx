0c7db80b877b0a20aa6348fbd11870be937408bf
ImMiguelP <anarchxzx@gmail.com>
2023-10-05 17:41:43 -0700

    feat: logo/name on nav goes to home page

--- src/components/NavBar/Nav.tsx
+++ src/components/NavBar/Nav.tsx
@@ -1,3 +1,4 @@
+import useZoom from '@/hooks/useZoom';
 import { Button, HStack, Text, useColorMode } from '@chakra-ui/react'
 import Link from 'next/link'
 import React from 'react'
@@ -5,17 +6,17 @@ import { FaMoon, FaSun } from 'react-icons/fa6';
 
 const Nav = () => {
     const { colorMode, toggleColorMode } = useColorMode();
+    const hoverEffect = useZoom();
   return (
     <HStack mx={5}  justifyContent={"space-between"}>
     {/* THis is where a logo would go if i had one so for now it will just be my name */}
-    {/* Uncomment Link Component when page goes live will redirect to home page */}
-        {/* <Link > */}
-        <Text>Miguel</Text>
-        {/* </Link> */}
+        <Link href={"/"} >
+        <Text {...hoverEffect}>Miguel</Text>
+        </Link>
         <HStack>
         <Text>test</Text>
         <Text>test</Text>
-        <Button variant="ghost" _hover={{ backgroundColor: "transparent" }} onClick={toggleColorMode}>
+        <Button variant="ghost" onClick={toggleColorMode} {...hoverEffect}>
         {colorMode === "light" ? <FaMoon /> : <FaSun />}
     </Button>
         </HStack>
