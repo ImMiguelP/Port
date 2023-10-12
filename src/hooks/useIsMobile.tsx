c5503a168a987ce268051402f94c6a3ce8ce1d5e
ImMiguelP <anarchxzx@gmail.com>
2023-10-12 13:03:29 -0700

    chore: hook created to check if user isMobile

---
+++ src/hooks/useIsMobile.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkIfMobile = () => {
+      if (window.innerWidth <= 1200) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    };
+
+    checkIfMobile();
+    window.addEventListener('resize', checkIfMobile);
+
+    return () => {
+      window.removeEventListener('resize', checkIfMobile);
+    };
+  }, []);
+
+  return isMobile;
+}
