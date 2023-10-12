4c2aa180ae4522e6c272b6a9444dd0ec66f7d0af
ImMiguelP <anarchxzx@gmail.com>
2023-10-12 12:40:48 -0700

    style: sticky buttons added to tech

--- src/components/Technologies/Technology.tsx
+++ src/components/Technologies/Technology.tsx
@@ -1,6 +1,7 @@
 import { useZoom } from '@/hooks/useStyling';
 import { Button, Tooltip } from '@chakra-ui/react';
 import React, { ReactNode } from 'react';
+import StickyButton from '../StickyButton';
 
 interface TechnologyProps {
     name: string;
@@ -12,9 +13,11 @@ const Technology: React.FC<TechnologyProps> = ({ name, icon, link }) => {
     const hoverEffect = useZoom();
   return (
     <Tooltip label={name}>
+      <StickyButton>
       <Button variant="ghost" as="a" href={link} target="_blank" rel="noopener noreferrer" {...hoverEffect}>
         {icon}
       </Button>
+      </StickyButton>
     </Tooltip>
   );
 };
