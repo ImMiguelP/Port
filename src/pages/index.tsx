ab33146d4a0aa3a1297b0ce97635390a6d7f6d9e
ImMiguelP <anarchxzx@gmail.com>
2023-10-15 16:18:45 -0700

    style: home page style change to take up whitspace

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,53 @@
-import {  Center, Heading, Stack, Link as ChakraLink   } from '@chakra-ui/react'
-import Technologies from '@/components/Technologies/Technologies'
-import Contact from '@/components/Contacts/Contact'
-import Projects from '@/components/ProjectInfo/Projects'
+import {
+  Center,
+  Heading,
+  Stack,
+  Link as ChakraLink,
+  Divider,
+} from "@chakra-ui/react";
+import Technologies from "@/components/Technologies/Technologies";
+import Contact from "@/components/Contacts/Contact";
+import Projects from "@/components/ProjectInfo/Projects";
 import { useIsMobile } from "@/hooks/useIsMobile";
 
 export default function Home() {
-  const isMobile = useIsMobile()
+  const isMobile = useIsMobile();
 
-  
   return (
-    <Center h={"100vh"} >
-      <Stack w={isMobile ? "90%" : "100%"} maxW={"1000px"} h={"100%"} pt={"200px"}  spacing={3}>
-      <Heading as={"h1"} size={"xl"}>
-        Miguel
-      </Heading>
-      <Heading as={"h5"} size={"sm"}>
-        28 year old with 4+ years of front web development. Currently building <ChakraLink href="https://musy.one" textDecoration="underline" color="blue">
-    musy
-  </ChakraLink>
-      </Heading>
-      <Heading pt={"12px"}>
-        Technologies
-      </Heading>
-        <Stack direction={"row"}>
-          <Technologies />
-          </Stack>
-          <Heading pt={"12px"}>
-        Projects
-      </Heading>
-      <Stack>
+    <Center h={"100vh"}>
+      <Stack
+        w={isMobile ? "90%" : "100%"}
+        maxW="1000px"
+        h="100%"
+        pt="200px"
+        spacing={3}
+      >
+        <Heading as="h1" size="xl">
+          Miguel
+        </Heading>
+        <Heading as="h5" size="sm">
+          28 year old with 4+ years of front web development. Currently building{" "}
+          <ChakraLink
+            href="https://musy.one"
+            textDecoration="underline"
+            color="blue"
+          >
+            musy
+          </ChakraLink>
+        </Heading>
+        <Heading py="50px">Tech I Use</Heading>
+        <Technologies />
+        <Divider pt="50px" />
+        <Center>
+          <Heading py="100px">Web Projects</Heading>
+        </Center>
         <Projects />
+        <Divider pt="50px" />
+        <Center>
+          <Heading py="50px">Contact Me</Heading>
+        </Center>
+        <Contact />
       </Stack>
-      <Heading pt={"12px"}>
-        Contact 
-      </Heading>
-      <Stack direction={"row"}>
-          <Contact />
-          </Stack>
-        </Stack>
     </Center>
-  )
+  );
 }
