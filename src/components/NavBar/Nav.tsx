5089b8a0dd126e33b38a2a1cda951ac516627c02
ImMiguelP <anarchxzx@gmail.com>
2023-10-16 23:51:12 -0700

    style: add blur to background on navbar

--- src/components/NavBar/Nav.tsx
+++ src/components/NavBar/Nav.tsx
@@ -3,6 +3,7 @@ import {
   Button,
   Center,
   Divider,
+  Flex,
   HStack,
   useColorMode,
   useColorModeValue,
@@ -13,11 +14,18 @@ import { FaMoon, FaSun } from "react-icons/fa6";
 import { RiHomeHeartFill } from "react-icons/ri";
 import { GiClown } from "react-icons/gi";
 import { SiMinutemailer } from "react-icons/si";
+import { useRouter } from "next/router";
 
 const Nav = () => {
   const { colorMode, toggleColorMode } = useColorMode();
-  const bgColor = useColorModeValue("gray.100", "dark");
+  const bgColor = useColorModeValue("light.200", "dark");
   const hoverEffect = useZoom();
+  const router = useRouter();
+  const buttons = [
+    { path: "/", icon: <RiHomeHeartFill /> },
+    { path: "/troll", icon: <GiClown /> },
+    { path: "/#contact-me", icon: <SiMinutemailer /> },
+  ];
 
   const handleMoonClick = () => {
     if (colorMode === "dark") {
@@ -32,46 +40,53 @@ const Nav = () => {
   };
 
   return (
-    <HStack w="100%" px={10} pos="fixed" height="100px">
+    <Flex
+      as="header"
+      pos="fixed"
+      top={0}
+      zIndex={1}
+      justify="center"
+      w="100%"
+      backdropFilter="blur(27px)"
+      height="80px"
+    >
       <Center w="100%">
-        <Link href="/">
-          <Button variant="ghost" {...hoverEffect}>
-            <RiHomeHeartFill />
-          </Button>
-        </Link>
-        <Link href="/troll">
-          <Button variant="ghost" {...hoverEffect}>
-            <GiClown />
-          </Button>
-        </Link>
-        <Link href="#contact-me">
-          <Button variant="ghost" {...hoverEffect}>
-            <SiMinutemailer />
-          </Button>
-        </Link>
+        {buttons.map(({ path, icon }) => (
+          <Link key={path} href={path}>
+            <Button
+              variant="ghost"
+              {...hoverEffect}
+              color={router.asPath === path ? "blue.300" : "gray.300"}
+            >
+              {icon}
+            </Button>
+          </Link>
+        ))}
         <Divider orientation="vertical" h={"20px"} />
         <HStack ml={1} bgColor={`${bgColor}`} borderRadius={"lg"}>
           <Center>
             <Button
               variant="ghost"
-              {...hoverEffect}
+              color={colorMode === "light" ? "orange.300" : ""}
               onClick={handleMoonClick}
               opacity={colorMode === "light" ? 1 : 0.5}
+              {...hoverEffect}
             >
               <FaSun />
             </Button>
             <Button
               variant="ghost"
-              {...hoverEffect}
+              color={colorMode === "dark" ? "teal.200" : ""}
               onClick={handleSunClick}
               opacity={colorMode === "dark" ? 1 : 0.5}
+              {...hoverEffect}
             >
               <FaMoon />
             </Button>
           </Center>
         </HStack>
       </Center>
-    </HStack>
+    </Flex>
   );
 };
 
