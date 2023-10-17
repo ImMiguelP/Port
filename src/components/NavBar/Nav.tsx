ca359db3e876648f54f18637726e4d74b5c21444
ImMiguelP <anarchxzx@gmail.com>
2023-10-16 23:18:36 -0700

    feat: add contact me button

--- src/components/NavBar/Nav.tsx
+++ src/components/NavBar/Nav.tsx
@@ -1,31 +1,39 @@
-import { useZoom } from '@/hooks/useStyling';
-import { Button, Center, Divider, HStack, useColorMode, useColorModeValue } from '@chakra-ui/react';
-import Link from 'next/link';
-import React from 'react';
-import { FaMoon, FaSun } from 'react-icons/fa6';
-import { RiHomeHeartFill } from 'react-icons/ri';
+import { useZoom } from "@/hooks/useStyling";
+import {
+  Button,
+  Center,
+  Divider,
+  HStack,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import React from "react";
+import { FaMoon, FaSun } from "react-icons/fa6";
+import { RiHomeHeartFill } from "react-icons/ri";
 import { GiClown } from "react-icons/gi";
+import { SiMinutemailer } from "react-icons/si";
 
 const Nav = () => {
   const { colorMode, toggleColorMode } = useColorMode();
-  const bgColor = useColorModeValue('gray.100', 'dark')
+  const bgColor = useColorModeValue("gray.100", "dark");
   const hoverEffect = useZoom();
 
   const handleMoonClick = () => {
-    if (colorMode === 'dark') {
+    if (colorMode === "dark") {
       toggleColorMode();
     }
   };
 
   const handleSunClick = () => {
-    if (colorMode === 'light') {
+    if (colorMode === "light") {
       toggleColorMode();
     }
   };
 
   return (
-    <HStack w="100%" px={10} pos="fixed" height='100px'>
-      <Center w="100%" >
+    <HStack w="100%" px={10} pos="fixed" height="100px">
+      <Center w="100%">
         <Link href="/">
           <Button variant="ghost" {...hoverEffect}>
             <RiHomeHeartFill />
@@ -36,16 +44,31 @@ const Nav = () => {
             <GiClown />
           </Button>
         </Link>
-        <Divider orientation='vertical' h={"20px"} />
+        <Link href="#contact-me">
+          <Button variant="ghost" {...hoverEffect}>
+            <SiMinutemailer />
+          </Button>
+        </Link>
+        <Divider orientation="vertical" h={"20px"} />
         <HStack ml={1} bgColor={`${bgColor}`} borderRadius={"lg"}>
-        <Center>
-        <Button variant="ghost" {...hoverEffect} onClick={handleMoonClick}  opacity={colorMode === 'light' ? 1 : 0.5}>
-            <FaSun />
-          </Button>
-          <Button variant="ghost" {...hoverEffect} onClick={handleSunClick} opacity={colorMode === 'dark' ? 1 : 0.5}>
-            <FaMoon />
-          </Button>
-        </Center>
+          <Center>
+            <Button
+              variant="ghost"
+              {...hoverEffect}
+              onClick={handleMoonClick}
+              opacity={colorMode === "light" ? 1 : 0.5}
+            >
+              <FaSun />
+            </Button>
+            <Button
+              variant="ghost"
+              {...hoverEffect}
+              onClick={handleSunClick}
+              opacity={colorMode === "dark" ? 1 : 0.5}
+            >
+              <FaMoon />
+            </Button>
+          </Center>
         </HStack>
       </Center>
     </HStack>
